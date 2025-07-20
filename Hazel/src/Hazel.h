958488ec5664a2ac294dfafe69cf438bd6aaf677
Load road for things <1754745919@qq.com>
2025-07-20 18:12:59 +0800

    complete 20

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -6,6 +6,10 @@
 #include "Hazel/Layer.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Input.h"
+#include "Hazel/KeyCode.h"
+#include "Hazel/MouseButtonCode.h"
+
 //---entry point-------------------
 #include"Hazel/EntryPoint.h"
 //------------------------------------------
